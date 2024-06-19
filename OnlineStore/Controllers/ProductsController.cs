47c6d3cd38ad02730a894d6bce9b99221d2d42cb
vanyok <vanya.zaytsev.04@gmail.com>
2024-06-19 11:47:58 +0300

    Добавлены контроллеры и папки моделей и сервисов

---
+++ OnlineStore/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.BLL.Models;
+using OnlineStore.BLL.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlineStore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+       
+    }
+}
