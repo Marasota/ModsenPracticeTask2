ac643914c5c804ff27c6eefd49585da1ca02c22a
Lavrinovich Maria <21it3.lavrinovich.m@pdu.by>
2024-06-21 12:42:39 +0300

    модели, контекст, fluent api

---
+++ OnlineStore.DAL/OnlineStoreDbContext.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.DAL.Entities;
+
+namespace OnlineStore.DAL
+{
+    public class OnlineStoreDbContext : DbContext
+    {
+        public OnlineStoreDbContext(DbContextOptions<OnlineStoreDbContext> options)
+            : base(options)
+        {
+          //  Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=OnlineStore;Trusted_Connection=True;"); //см в appsittings
+            }
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.ProductId);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.HasOne(e => e.Category)
+                      .WithMany(c => c.Products)
+                      .HasForeignKey(e => e.CategoryId)
+                // При удалении Category, CategoryId становится null
+                      .OnDelete(DeleteBehavior.SetNull); 
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(e => e.OrderId);
+                entity.HasOne(e => e.User)
+                      .WithMany(u => u.Orders)
+                      .HasForeignKey(e => e.UserId)
+                // При удалении User, каскадно удаляются связанные Order
+                      .OnDelete(DeleteBehavior.Cascade); 
+                entity.Property(e => e.OrderDate).IsRequired();
+            });
+
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.HasKey(e => e.OrderItemId);
+                entity.HasOne(e => e.Order)
+                      .WithMany(o => o.OrderItems)
+                      .HasForeignKey(e => e.OrderId)
+                // При удалении Order, каскадно удаляются связанные OrderItem
+                      .OnDelete(DeleteBehavior.Cascade); 
+                entity.HasOne(e => e.Product)
+                      .WithMany()
+                      .HasForeignKey(e => e.ProductId)
+                // При удалении Product, каскадно удаляются связанные OrderItem
+                      .OnDelete(DeleteBehavior.Cascade); 
+                entity.Property(e => e.Quantity).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+                entity.Property(e => e.UserName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.PasswordHash).IsRequired();
+            });
+        }
+
+    }
+}
